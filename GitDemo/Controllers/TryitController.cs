50a3ff188ef0dd41056cd8d76784394bf55f684b
tryit296923 <tryit296923@gmail.com>
2022-10-06 14:17:24 +0800

    add tritCTRL

---
+++ GitDemo/Controllers/TryitController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitDemo.Controllers
+{
+    public class TryitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public string Hello()
+        {
+            return "Reds88";
+        }
+    }
+}
