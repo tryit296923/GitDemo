6238501a3235d9a27143cc0503701e134da2eb94
Luka <2221kane@gmail.com>
2022-10-21 09:35:54 +0800

    create LukaController

---
+++ GitDemo/Controllers/LukaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitDemo.Controllers
+{
+    public class LukaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
