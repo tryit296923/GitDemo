069a2adc1dc3d0c6647c4cc64987893ecb53aa75
Vincent <tyrionv9527@gmail.com>
2022-10-21 09:39:47 +0800

    Edit Vincent controller name &page Edit program.cs

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10800);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
